e5964740dbdb49d3e95b981d4a219a280633649c
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2020-10-07 00:23:45 -0700

    random color picker

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Random from './Random';
+import Button from './Button';
 
 function App() {
   return (
     <div className="App">
-      React fun lab
+      <Random />
+      <Button />
     </div>
   );
 }
